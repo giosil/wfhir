c288dde1880adda02ffa899a0ac65ee617daa2aa
Giorgio Silvestris <giorgio.silvestris@gmail.com>
2022-03-14 12:33:42 +0100

    update

---
+++ src/main/java/org/dew/fhir/xml/XmlSerializer.java
@@ -0,0 +1,271 @@
+package org.dew.fhir.xml;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+import java.time.ZoneId;
+
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+
+public
+class XmlSerializer
+{
+  public static
+  String serialize(Object value)
+  {
+    if(value == null) return "";
+    
+    Class<?> klass = value.getClass();
+    String className = klass.getName();
+    if(className.startsWith("java.")) {
+      return "";
+    }
+    String tagName = klass.getSimpleName();
+    
+    StringBuilder result = new StringBuilder();
+    result.append("<" + tagName + " xmlns=\"http://hl7.org/fhir\">");
+    // If klass is a System class then set includeSuperClass to false.
+    boolean includeSuperClass = klass.getClassLoader() != null;
+    Method[] methods = includeSuperClass ? klass.getMethods() : klass.getDeclaredMethods();
+    for(int i = 0; i < methods.length; i++) {
+      try {
+        Method method = methods[i];
+        if(Modifier.isPublic(method.getModifiers())) {
+          String name = method.getName();
+          String key  = "";
+          if(name.startsWith("get")) {
+            if("getClass".equals(name) || "getDeclaringClass".equals(name)) {
+              key = "";
+            }
+            else {
+              key = name.substring(3);
+            }
+          }
+          else if(name.startsWith("is")) {
+            key = name.substring(2);
+          }
+          if(key.length() > 0 && key.charAt(0) < 97 && method.getParameterTypes().length == 0) {
+            if(key.length() == 1) {
+              key = key.toLowerCase();
+            }
+            else if(!Character.isUpperCase(key.charAt(1))) {
+              key = key.substring(0, 1).toLowerCase() + key.substring(1);
+            }
+            Object oValue = method.invoke(value, (Object[]) null);
+            if(oValue != null) {
+              result.append(value(key, oValue));
+            }
+          }
+        }
+      }
+      catch(Exception ex) {
+      }
+    }
+    result.append("</" + tagName + ">");
+    return result.toString();
+  }
+  
+  private static
+  String value(String tagName, Object value)
+  {
+    if(value == null || tagName.length() == 0 || tagName.equals("resourceType")) return "";
+    if(tagName.endsWith("_")) {
+      tagName = tagName.substring(0, tagName.length()-1);
+    }
+    Class<?> klass = value.getClass();
+    
+    StringBuilder result = new StringBuilder();
+    
+    if(value instanceof Map) {
+      Map<?, ?> map = (Map<?, ?>) value;
+      Object urlValue = map.get("url");
+      if(urlValue != null) {
+        result.append("<" + tagName + " url=\"" + urlValue + "\">");
+      }
+      else {
+        result.append("<" + tagName + ">");
+      }
+      Iterator<? extends Map.Entry<?, ?>> iterator = map.entrySet().iterator();
+      while(iterator.hasNext()) {
+        Map.Entry<?, ?> entry = iterator.next();
+        String key = entry.getKey().toString();
+        if(key.equals("url")) continue;
+        result.append(value(key, entry.getValue()));
+      }
+      result.append("</" + tagName + ">");
+    }
+    else if(value instanceof Collection) {
+      Collection<?> collection = (Collection<?>) value;
+      Iterator<?> iterator = collection.iterator();
+      while(iterator.hasNext()) {
+        result.append(value(tagName, iterator.next()));
+      }
+    }
+    else if(value.getClass().isArray()) {
+      int length = Array.getLength(value);
+      for(int i = 0; i < length; i++) {
+        result.append(value(tagName, Array.get(value, i)));
+      }
+    }
+    else {
+      String valueClassName = klass.getName();
+      if(valueClassName.startsWith("java.")) {
+        if(value instanceof Date) {
+          result.append("<" + tagName + " value=\"" + dateTimeToString(value)  + "\"/>");
+        }
+        else if(value instanceof Calendar) {
+          result.append("<" + tagName + " value=\"" + dateTimeToString(value) + "\"/>");
+        }
+        else if(value instanceof java.time.LocalDate) {
+          result.append("<" + tagName + " value=\"" + dateTimeToString(value) + "\"/>");
+        }
+        else if(value instanceof java.time.LocalDateTime) {
+          result.append("<" + tagName + " value=\"" + dateTimeToString(value) + "\"/>");
+        }
+        else if(tagName.equals("div")) {
+          result.append(value);
+        }
+        else {
+          result.append("<" + tagName + " value=\"" + normalizeAttribute(value.toString()) + "\"/>");
+        }
+      }
+      else {
+        result.append("<" + tagName + ">");
+        // If klass is a System class then set includeSuperClass to false.
+        boolean includeSuperClass = klass.getClassLoader() != null;
+        Method[] methods = includeSuperClass ? klass.getMethods() : klass.getDeclaredMethods();
+        for(int i = 0; i < methods.length; i++) {
+          try {
+            Method method = methods[i];
+            if(Modifier.isPublic(method.getModifiers())) {
+              String name = method.getName();
+              String key  = "";
+              if(name.startsWith("get")) {
+                if("getClass".equals(name) || "getDeclaringClass".equals(name)) {
+                  key = "";
+                }
+                else {
+                  key = name.substring(3);
+                }
+              }
+              else if(name.startsWith("is")) {
+                key = name.substring(2);
+              }
+              if(key.length() > 0 && key.charAt(0) < 97 && method.getParameterTypes().length == 0) {
+                if(key.length() == 1) {
+                  key = key.toLowerCase();
+                }
+                else if(!Character.isUpperCase(key.charAt(1))) {
+                  key = key.substring(0, 1).toLowerCase() + key.substring(1);
+                }
+                Object oValue = method.invoke(value, (Object[]) null);
+                if(oValue != null) {
+                  result.append(value(key, oValue));
+                }
+              }
+            }
+          }
+          catch(Exception ex) {
+          }
+        }
+        result.append("</" + tagName + ">");
+      }
+    }
+    return result.toString();
+  }
+  
+  private static
+  String normalizeAttribute(String value)
+  {
+    if(value == null) return "null";
+    StringBuilder sbResult = new StringBuilder(value.length());
+    int iLength = value.length();
+    for(int i = 0; i < iLength; i++) {
+      char c = value.charAt(i);
+      switch(c) {
+        case '<':
+          sbResult.append("&lt;");
+          break;
+        case '>':
+          sbResult.append("&gt;");
+          break;
+        case '"':
+          sbResult.append("&quot;");
+          break;
+        case '&':
+          sbResult.append("&amp;");
+          break;
+        default:
+          sbResult.append(c);
+      }
+    }
+    return sbResult.toString();
+  }
+  
+  private static
+  String dateTimeToString(Object value)
+  {
+    if(value == null) return "";
+    
+    Calendar cal = null;
+    if(value instanceof java.util.Date) {
+      cal = Calendar.getInstance();
+      cal.setTimeInMillis(((java.util.Date) value).getTime());
+    }
+    else if(value instanceof java.util.Calendar) {
+      cal = (Calendar) value;
+    }
+    else if(value instanceof java.time.LocalDate) {
+      cal = Calendar.getInstance();
+      cal.setTimeInMillis(((java.time.LocalDate) value).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+    }
+    else if(value instanceof java.time.LocalDateTime) {
+      cal = Calendar.getInstance();
+      cal.setTimeInMillis(((java.time.LocalDateTime) value).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+    }
+    
+    // Get total offset in minutes
+    int iZoneOffsetMin = cal.get(Calendar.ZONE_OFFSET) / 60000;
+    int iDST_OffsetMin = cal.get(Calendar.DST_OFFSET)  / 60000;
+    int iTot_OffsetMin = iZoneOffsetMin + iDST_OffsetMin;
+    boolean negOffset  = false;
+    if(iTot_OffsetMin < 0) {
+      iTot_OffsetMin = iTot_OffsetMin * -1;
+      negOffset = true;
+    }
+    // Format offset
+    int iTot_OffsetHH = iTot_OffsetMin / 60;
+    int iTot_OffsetMM = iTot_OffsetMin % 60;
+    String sOffsetHH  = iTot_OffsetHH < 10 ? "0" + iTot_OffsetHH : String.valueOf(iTot_OffsetHH);
+    String sOffsetMM  = iTot_OffsetMM < 10 ? "0" + iTot_OffsetMM : String.valueOf(iTot_OffsetMM);
+    String sOffset = negOffset ? "-" + sOffsetHH + ":" + sOffsetMM : "+" + sOffsetHH + ":" + sOffsetMM;
+    
+    int iYear  = cal.get(Calendar.YEAR);
+    int iMonth = cal.get(Calendar.MONTH) + 1;
+    int iDay   = cal.get(Calendar.DATE);
+    int iHour  = cal.get(Calendar.HOUR_OF_DAY);
+    int iMin   = cal.get(Calendar.MINUTE);
+    int iSec   = cal.get(Calendar.SECOND);
+    String sYear  = String.valueOf(iYear);
+    String sMonth = iMonth < 10 ? "0" + iMonth : String.valueOf(iMonth);
+    String sDay   = iDay   < 10 ? "0" + iDay   : String.valueOf(iDay);
+    String sHour  = iHour  < 10 ? "0" + iHour  : String.valueOf(iHour);
+    String sMin   = iMin   < 10 ? "0" + iMin   : String.valueOf(iMin);
+    String sSec   = iSec   < 10 ? "0" + iSec   : String.valueOf(iSec);
+    if(iYear < 10) {
+      sYear = "000" + sYear;
+    }
+    else if(iYear < 100) {
+      sYear = "00" + sYear;
+    }
+    else if(iYear < 1000) {
+      sYear = "0" + sYear;
+    }
+    return sYear + "-" + sMonth + "-" + sDay + "T" + sHour + ":" + sMin + ":" + sSec + sOffset;
+  }
+}
--- src/test/java/org/dew/test/TestWFHIR.java
+++ src/test/java/org/dew/test/TestWFHIR.java
@@ -1,6 +1,8 @@
 package org.dew.test;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import org.dew.fhir.model.Address;
 import org.dew.fhir.model.CodeableConcept;
@@ -11,9 +13,12 @@ import org.dew.fhir.model.Element;
 import org.dew.fhir.model.Identifier;
 import org.dew.fhir.model.Narrative;
 import org.dew.fhir.model.Organization;
+import org.dew.fhir.model.Resource;
 import org.dew.fhir.model.ValueSet;
 import org.dew.fhir.model.ValueSetCompose;
 import org.dew.fhir.model.ValueSetComposeInclude;
+
+import org.dew.fhir.xml.XmlSerializer;
 
 import org.dew.fhir.json.JSON;
 
@@ -31,18 +36,30 @@ public class TestWFHIR extends TestCase {
     return new TestSuite(TestWFHIR.class);
   }
   
-  public void testApp() {
-    ValueSet valueSet = buildValueSetIstatDug();
+  public 
+  void testApp() 
+  {
+    Resource resource = buildValueSetIstatDug();
     
-    String jsonValueSet = JSON.stringify(valueSet);
+    doSerialize(resource);
     
-    System.out.println(jsonValueSet);
+    resource = buildTestOrganization();
     
-    Organization organization = buildTestOrganization();
+    doSerialize(resource);
+  }
+  
+  protected
+  void doSerialize(Object resource)
+  {
+    String serialized = null;
     
-    String jsonOrganization = JSON.stringify(organization);
+    System.out.println(resource + " -> xml...");
+    serialized = XmlSerializer.serialize(resource);
+    System.out.println(serialized);
     
-    System.out.println(jsonOrganization);
+    System.out.println(resource + " -> json...");
+    serialized = JSON.stringify(resource);
+    System.out.println(serialized);
   }
   
   protected
@@ -53,9 +70,9 @@ public class TestWFHIR extends TestCase {
     organization.setText(new Narrative("generated", "<div xmlns=\"http://www.w3.org/1999/xhtml\"><p><b>Generated Narrative</b></p><p><b>id</b>: asl-020101</p><p><b>identifier</b>: 020101, 101, 00177330073, 91001750073</p><p><b>active</b>: true</p><p><b>type</b>: <span title=\"Codes: {http://terminology.hl7.it/CodeSystem/it-tipoEntita asl}\">Azienda Sanitaria Locale</span></p><p><b>name</b>: AZIENDA U.S.L. VALLE D'AOSTA</p><p><b>telecom</b>: ph: (0165) 5431, fax: (0165) 544587, mailto: protocollo@pec.ausl.vda.it, http://www.ausl.vda.it</p><p><b>address</b>: VIA GUIDO REY 1 AOSTA VALLE D'AOSTA 11100 IT </p></div>"));
     organization.setIdentifier(buildOrganizationIdentifier("020101", "101", "00177330073", "91001750073"));
     organization.setActive(true);
-    organization.setType(buildOrganizationType("http://terminology.hl7.it/CodeSystem/it-tipoEntita", "asl", "Azienda Sanitaria Locale"));
+    organization.setType(buildArrayOfCodeableConcept("http://terminology.hl7.it/CodeSystem/it-tipoEntita", "asl", "Azienda Sanitaria Locale"));
     organization.setName("AZIENDA U.S.L. VALLE D'AOSTA");
-    organization.setTelecom(buildOrganizationTelecom("(0165) 5431", "(0165) 544587", "mailto: protocollo@pec.ausl.vda.it", "http://www.ausl.vda.it"));
+    organization.setTelecom(buildArrayOfContactPoint("(0165) 5431", "(0165) 544587", "mailto: protocollo@pec.ausl.vda.it", "http://www.ausl.vda.it"));
     
     Address address = new Address("VIA GUIDO REY 1", "AOSTA", "AO", "VALLE D'AOSTA", "11100", "IT");
     address.set_city(new Element("http://hl7.org/fhir/StructureDefinition/iso21090-SC-coding", "http://terminology.hl7.it/CodeSystem/istat-unitaAmministrativeTerritoriali", "007003", "AOSTA"));
@@ -79,7 +96,7 @@ public class TestWFHIR extends TestCase {
     valueSet.setStatus("active");
     valueSet.setDate(new GregorianCalendar(2020, 5, 30, 18, 23, 29).getTime());
     valueSet.setPublisher("HL7 Italia");
-    valueSet.setContact(buildValueSetContact("url", "http://hl7.it"));
+    valueSet.setContact(buildArrayOfContactDetail("url", "http://hl7.it"));
     valueSet.setDescription("Registro delle Denominazioni Urbanistiche Generiche (DUG)");
     valueSet.setJurisdiction(buildValueSetJurisdiction("urn:iso:std:iso:3166", "IT"));
     valueSet.setCompose(buildValueSetCompose("http://registry.geodati.gov.it/dug"));
@@ -87,7 +104,7 @@ public class TestWFHIR extends TestCase {
   }
   
   protected 
-  ContactDetail[] buildValueSetContact(String telecomSystem, String telecomValue)
+  ContactDetail[] buildArrayOfContactDetail(String telecomSystem, String telecomValue)
   {
     ContactPoint contactPoint = new ContactPoint(telecomSystem, telecomValue);
     
@@ -120,18 +137,18 @@ public class TestWFHIR extends TestCase {
   }
   
   protected
-  Identifier[] buildOrganizationIdentifier(String fls, String asl, String partitaIva, String codiceFiscale)
+  Identifier[] buildOrganizationIdentifier(String codiceMin, String codiceReg, String partitaIva, String codiceFiscale)
   {
-    Identifier id0 = new Identifier("http://hl7.it/sid/fls", fls);
-    Identifier id1 = new Identifier("http://hl7.it/sid/vda/asl", asl);
-    Identifier id2 = new Identifier("http://hl7.it/sid/partitaIva", partitaIva);
+    Identifier id0 = new Identifier("http://hl7.it/sid/fls",           codiceMin);
+    Identifier id1 = new Identifier("http://hl7.it/sid/vda/asl",       codiceReg);
+    Identifier id2 = new Identifier("http://hl7.it/sid/partitaIva",    partitaIva);
     Identifier id3 = new Identifier("http://hl7.it/sid/codiceFiscale", codiceFiscale);
     
     return new Identifier[] { id0, id1, id2, id3 };
   }
   
   protected 
-  CodeableConcept[] buildOrganizationType(String system, String code, String display)
+  CodeableConcept[] buildArrayOfCodeableConcept(String system, String code, String display)
   {
     Coding coding = new Coding(system, code, display);
     
@@ -142,13 +159,29 @@ public class TestWFHIR extends TestCase {
   }
   
   protected 
-  ContactPoint[] buildOrganizationTelecom(String phone, String fax, String email, String url)
+  ContactPoint[] buildArrayOfContactPoint(String phone, String fax, String email, String url)
   {
-    ContactPoint cp0 = new ContactPoint("phone", phone);
-    ContactPoint cp1 = new ContactPoint("fax", fax);
-    ContactPoint cp2 = new ContactPoint("email", email);
-    ContactPoint cp3 = new ContactPoint("url", url);
+    List<ContactPoint> listContactPoint = new ArrayList<ContactPoint>();
     
-    return new ContactPoint[] { cp0, cp1, cp2, cp3 };
+    if(phone != null && phone.length() > 3) {
+      listContactPoint.add(new ContactPoint("phone", phone));
+    }
+    if(fax != null && fax.length() > 3) {
+      listContactPoint.add(new ContactPoint("fax", phone));
+    }
+    if(email != null && email.length() > 5 && email.indexOf('@') > 0 && email.indexOf('.') > 0) {
+      listContactPoint.add(new ContactPoint("email", phone));
+    }
+    if(url != null && url.length() > 5) {
+      listContactPoint.add(new ContactPoint("url",   url));
+    }
+    
+    ContactPoint[] result = new ContactPoint[listContactPoint.size()];
+    
+    for(int i = 0; i < listContactPoint.size(); i++) {
+      result[i] = listContactPoint.get(i);
+    }
+    
+    return result;
   }
 }
