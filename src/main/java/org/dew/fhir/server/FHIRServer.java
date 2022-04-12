0f1e8c1b0fb3ac762358a2a75873b9eb2472426e
Giorgio Silvestris <giorgio.silvestris@gmail.com>
2022-04-12 18:12:52 +0200

    update

--- src/main/java/org/dew/fhir/server/FHIRServer.java
+++ src/main/java/org/dew/fhir/server/FHIRServer.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.dew.fhir.model.Resource;
+import org.dew.fhir.services.FHIRRequest;
+import org.dew.fhir.services.FHIRResponse;
+
 /**
  * 
  * FHIR Server.
@@ -17,6 +21,7 @@ class FHIRServer extends HttpServlet
 {
   private static final long serialVersionUID = -8635635498516268187L;
   
+  @Override
   public 
   void init() 
     throws ServletException 
@@ -24,6 +29,21 @@ class FHIRServer extends HttpServlet
     // TODO Auto-generated method stub
   }
   
+  @Override
+  protected 
+  void service(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException
+  {
+    String method = request.getMethod();
+    if(method.equals("PATCH")) {
+      doPatch(request, response);
+    }
+    else {
+      super.service(request, response);
+    }
+  }
+  
+  @Override
   public
   void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException
@@ -31,10 +51,47 @@ class FHIRServer extends HttpServlet
     // TODO Auto-generated method stub
   }
   
+  @Override
   public
   void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException
   {
     // TODO Auto-generated method stub
+  }
+  
+  @Override
+  public
+  void doPut(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException
+  {
+    // TODO Auto-generated method stub
+  }
+  
+  @Override
+  public
+  void doDelete(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException
+  {
+    // TODO Auto-generated method stub
+  }
+  
+  public
+  void doPatch(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException
+  {
+    // TODO Auto-generated method stub
+  }
+  
+  protected
+  FHIRRequest<Resource> createFHIRRequest(HttpServletRequest request, HttpServletResponse response)
+  {
+    return new FHIRRequest<Resource>();
+  }
+  
+  protected
+  void send(HttpServletRequest request, HttpServletResponse response, FHIRResponse<?> fhirResponse)
+    throws ServletException, IOException
+  {
+    
   }
 }
